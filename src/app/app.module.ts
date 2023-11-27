e5445f922a9dd33f749770b264a27dfd0b3beee0
Fabian <fabirir99@gmail.com>
2023-11-27 14:52:42 -0500

    cvvd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import {AngularFireModule} from '@angular/fire/compat';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
